8247608d4aa426fa89c69d646fd0702c41183431
Faith-Kimongo <faithvictor78@gmail.com>
2023-10-19 14:29:50 +0300

    js for board members added

--- RREC-Landing-Page/index.js
+++ RREC-Landing-Page/index.js
@@ -70,3 +70,64 @@ const images = [
   }
 //
 //
+const boardMembers = [
+    {
+        name: "John Doe",
+        imageSrc: "/images/board/Mask group (1).png",
+    },
+    {
+        name: "Jane Smith",
+        imageSrc: "/images/board/Mask group.png",
+    },
+    {
+        name: "Bob Johnson",
+        imageSrc: "/images/board/Mask group (2).png",
+    },
+    {
+        name: "Sarah Adams",
+        imageSrc: "/images/board/Mask group (3).png",
+    },
+    // Add more board members as needed
+];
+
+let currentBoardIndex = 0;
+const boardCardGroupSizeLarge = 4; // Display four cards on larger screens
+const boardCardGroupSizeSmall = 2; // Display two cards on smaller screens
+
+function changeBoardMember(direction) {
+    const boardCardGroupSize = window.innerWidth < 768 ? boardCardGroupSizeSmall : boardCardGroupSizeLarge;
+
+    currentBoardIndex += direction;
+
+    if (currentBoardIndex < 0) {
+        currentBoardIndex = boardMembers.length - 1;
+    } else if (currentBoardIndex >= boardMembers.length) {
+        currentBoardIndex = 0;
+    }
+
+    const boardCards = document.querySelectorAll('.board-card');
+    for (let i = 0; i < boardCards.length; i++) {
+        const index = (currentBoardIndex + i) % boardMembers.length;
+        const card = boardCards[i];
+        const member = boardMembers[index];
+
+        card.innerHTML = `
+            <img src="${member.imageSrc}" alt="${member.name}">
+            <h2>${member.name}</h2>
+        `;
+    }
+}
+
+// Call the function on page load to show the initial set of board members
+changeBoardMember(0);
+
+
+
+
+
+
+
+
+
+
+
