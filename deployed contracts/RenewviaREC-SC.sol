7c84657e7f2b1ed51386da5d5c3c0449215d5b1e
Nicholas Selby <rupumped@gmail.com>
2023-10-28 19:16:48 +0300

    add SC

---
+++ deployed contracts/RenewviaREC-SC.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPLv3
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact technical-africa@renewvia.com
+contract RenewviaREC is ERC20, Pausable, Ownable {
+    constructor(uint256 premint) ERC20("RenewviaREC-SouthCarolina", "RREC-SC") {
+        _mint(msg.sender, premint * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount * 10 ** decimals());
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+}
