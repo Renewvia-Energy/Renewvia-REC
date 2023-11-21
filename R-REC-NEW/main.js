e2514bcca162bec3636e432ced5234a1745d4ba9
Faith-Kimongo <faithvictor78@gmail.com>
2023-11-21 16:34:46 +0300

    new updates, unwanted files removed

--- R-REC-NEW/main.js
+++ R-REC-NEW/main.js
@@ -1,143 +1,134 @@
-
 const app = Vue.createApp({
-    data() {
-        return {
-            isHidden: true,
-            carbonAssetsData: [],
-            allTransactions: [],
-            statesToDisplay: ["Virginia R-REC", "Alabama R-REC", "Georgia R-REC", "North Carolina R-REC", "SteelFab"],
-            newStates: [],
-            doughnutData: {},
-            chart: null,
-            totalRenewableEnergy: 0,
-            totalCarbonOffsets: 0,
-            };
-        },
-    
-    mounted() {
-        this.statesData();
-    },
-    updated() {
-    // Check if the chart instance exists and destroy it
-    if (this.chart) {
-      this.chart.destroy();
+  data() {
+    return {
+      isHidden: true,
+      carbonAssetsData: [],
+      companyData: [],
+      doughnutData: {},
+      chart: null,
+      totalRenewableEnergy: 0,
+      totalCarbonOffsets: 0,
+    };
+  },
+
+  mounted() {
+    this.statesData();
+  },
+  updated() {
+    if (!this.chart) {
+      // Render the chart with the new data
+      this.renderChart();
     }
-    // Render the chart with the new data
-    this.renderChart();
+  },
+  methods: {
+    statesData() {
+      const companyAddress = window.location.href.split("?")[1];
+      const contracts_url =
+        "https://raw.githubusercontent.com/Faith-Kimongo/Renewvia-REC/main/R-REC-NEW/contracts.json";
+      const companies_url =
+        "https://raw.githubusercontent.com/Faith-Kimongo/Renewvia-REC/main/R-REC-NEW/companies.json";
+
+      // Get all the contracts from json in github
+      fetch(contracts_url)
+        .then((allContractsResp) => allContractsResp.json())
+        .then((allContractsData) => {
+          // loop through each contract and add key for qyt and transactions
+          for (let i = 0; i < allContractsData.length; i++) {
+            // set the key values for qty to 0 and transaction to empty array
+            allContractsData[i]["chartData"] = {};
+          }
+
+          // filter the contracts if company address is passed in the url
+          if (companyAddress) {
+            this.carbonAssetsData = allContractsData.filter(
+              (asset) => asset.company_address === companyAddress
+            );
+            // getting company data
+            fetch(companies_url)
+              .then((allCompaniesResp) => allCompaniesResp.json())
+              .then((allCompanyData) => {
+                for (let i = 0; i < allCompanyData.length; i++) {
+                  if (allCompanyData[i].address === companyAddress) {
+                    this.companyData.push(allCompanyData[i]);
+                  }
+                }
+              });
+          }
+          // if no company address is passed on the url return all the contracts
+          if (!companyAddress) {
+            this.carbonAssetsData = allContractsData;
+          }
+
+          // call function(method) to calculate renewable energy Total
+          this.calculateRenewableEnergy();
+
+          //passing the data to the chart
+          for (let contract of this.carbonAssetsData) {
+            const labels = this.carbonAssetsData.map((asset) => asset.name);
+            const quantities = this.carbonAssetsData.map((asset) => asset.qty);
+
+            this.doughnutData = {
+              labels: labels,
+              datasets: [
+                {
+                  backgroundColor: [
+                    "#FF5733",
+                    "rgb(239, 216, 6)",
+                    "#ff6d05",
+                    "rgb(218, 148, 68)",
+                  ], // Add colors as needed
+                  data: quantities,
+                },
+              ],
+            };
+            contract.chartData = this.doughnutData;
+          }
+        });
+    },
+    calculatePercentage(quantity) {
+      // Calculate the percentage here based on the total quantity
+      if (quantity === 0) {
+        return 0;
+      }
+      const totalQuantity = this.carbonAssetsData.reduce(
+        (total, asset) => total + asset.qty,
+        0
+      );
+      const answer = Math.round((quantity / totalQuantity) * 100);
+      return answer;
     },
 
-    methods: {
-        statesData(){
-    
-          // declear required variables to use on api call
-          const API_KEY = "FV7VI88AT9SBQDZU3WYWGPWZNVDUEY9RTW"
-          const WALLET_ADDR = "0x9Db94E89DB9798544494a71C01E3552D6adE79bE"
-          const stateAddress = window.location.href.split('?')[1]
-          const url = 'https://raw.githubusercontent.com/Faith-Kimongo/Renewvia-REC/main/contracts.json'
-    
-          // Get all the state from json in github
-          fetch(url).then(allContractsResp => allContractsResp.json()).then(allContractsData => { 
-              // loop through each state and add key for qyt and transactions
-              for (let i = 0; i < allContractsData.length; i++){
-                // set the key values for qty to 0 and transaction to empty array
-                allContractsData[i]['qty'] = 0;
-                allContractsData[i]['transactions'] = [];
-                allContractsData[i]['chartData'] = {};
-              }
-              // filter the states if state address is passed in the url
-              if (stateAddress){
-                this.carbonAssetsData = allContractsData.filter((asset)=> asset.address === stateAddress);
-              }
-              // if no adress is passed on the url return all the states (filter by statesToDisplay list)
-              if (!stateAddress){
-                        this.carbonAssetsData = allContractsData.filter((asset)=>this.statesToDisplay.includes(asset.name));
-              } 
-    
-                // get token quantity
-                for (let contract of  this.carbonAssetsData){
-                  // set time out
-                  setTimeout(() => {
-                    fetch(`https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${contract.address}&address=${WALLET_ADDR}&tag=latest&apikey=${API_KEY}`).then(resp => resp.json()).then(data => {
-                      contract.qty = data.result.slice(0, -18)
-                      
-                      const labels = this.carbonAssetsData.map((asset) => asset.name);
-                      const quantities = this.carbonAssetsData.map((asset) => asset.qty);
-    
-                      this.doughnutData = {
-                        labels: labels,
-                        datasets: [
-                          {
-                            backgroundColor: ['#FF5733', 'rgb(239, 216, 6)', '#ff6d05', 'rgb(218, 148, 68)'], // Add colors as needed
-                            data: quantities,
-                          },]};
-                      contract.chartData = this.doughnutData
-                      });
-                      }, parseInt(contract/4)*1000)
-                }
-            });
-            
-            // get all transactions for REC wallet
-            fetch(`https://api.bscscan.com/api?module=account&action=tokentx&address=${WALLET_ADDR}&page=1&offset=0&startblock=0&endblock=999999999&sort=asc&apikey=${API_KEY}`).then(resp => resp.json()).then(data => {this.allTransactions = data.result
-            
-            // Group transactions as per their contract address
-            for (let transaction of this.allTransactions) {
-              for (let contract of  this.carbonAssetsData){
-                // check if the transaction.contractAddress matches contract.address, add to contract.transaction[]
-                if(transaction.contractAddress === contract.address){
-                  contract.transactions.push(transaction)
-                  break
-                }
-              }
-            }
-            console.log(this.carbonAssetsData)
-            this.calculateToken()
-
-            });
-        }, 
-        calculatePercentage(quantity) {
-              // Calculate the percentage here based on the total quantity
-              if (quantity === 0) {
-                  return 0;
-                  }
-                  const totalQuantity = this.carbonAssetsData.reduce((total, asset) => total + asset.qty, 0);
-                  return ((quantity / totalQuantity) * 100).toExponential(2);      
-          },
-
-        calculateToken(){
-          let total = 0
-          for (let asset of this.carbonAssetsData){
-            if (asset.transactions.length > 0){
-              console.log(asset.name)
-              for(let transaction of asset.transactions){
-                let token = transaction.value.slice(0, 4)
-                token = parseInt(token)
-                total = total + token
-              }
-            }
-          }
-          this.totalRenewableEnergy = total
-        },
-        
-        renderChart() {
-            if (this.$refs.myChart && this.carbonAssetsData && this.carbonAssetsData.length > 0) {
-                const ctx = this.$refs.myChart.getContext('2d');
-            if (ctx) {
-                this.chart = new Chart(ctx, {
-                    type: 'doughnut',
-                    data: this.carbonAssetsData[this.carbonAssetsData.length - 1].chartData,
-                    options: {
-                        responsive: true,
-                        maintainAspectRatio: false,
-                    },
-                });
-            }
+    calculateRenewableEnergy() {
+      let total = 0;
+      for (let asset of this.carbonAssetsData) {
+        // calculating renewable energy total using hardcoded value
+        total = total + asset.qty;
+      }
+      this.totalRenewableEnergy = total;
+    },
+    renderChart() {
+      if (
+        this.$refs.myChart &&
+        this.carbonAssetsData &&
+        this.carbonAssetsData.length > 0
+      ) {
+        const ctx = this.$refs.myChart.getContext("2d");
+        if (ctx) {
+          this.chart = new Chart(ctx, {
+            type: "doughnut",
+            data: this.carbonAssetsData[this.carbonAssetsData.length - 1]
+              .chartData,
+            options: {
+              responsive: true,
+              maintainAspectRatio: false,
+            },
+          });
         }
-        },
-        toggleMenu(){
-          this.isHidden = !this.isHidden;
-        },
-    
-      },
+      }
+    },
+    toggleMenu() {
+      this.isHidden = !this.isHidden;
+    },
+  },
 });
-app.mount('#app')
-
+app.mount("#app");
---
+++ R-REC-NEW/updateData.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+# update contracts json data with information from api
+
+import requests
+import json
+import os
+
+# kets to use in the Api call
+API_KEY = "FV7VI88AT9SBQDZU3WYWGPWZNVDUEY9RTW"
+WALLET_ADDR = "0x9Db94E89DB9798544494a71C01E3552D6adE79bE"
+
+# get keys from environment variables
+# set environment variable  < export API_KEY="your key">
+# API_KEY = os.environ.get('API_KEY')
+# WALLET_ADDR = os.environ.get("WALLET_ADDR")
+
+# pass keys as arguments when running the script  => ./updateData <your API_KEY> <your WALLET_address>
+# API_KEY = os.sys.argv[1]
+# WALLET_ADDR = os.sys.argv[2]
+
+# url to get all contracts from github
+contractsUrl = 'https://raw.githubusercontent.com/Faith-Kimongo/Renewvia-REC/main/R-REC-NEW/contracts.json'
+
+print('Getting data from github ...')
+
+# use requests module to send get request and get json data from github contracts.json file
+response = requests.get(contractsUrl)
+allContracts = response.json()
+
+# transaction url for api
+TRANSACTION_URL = f"https://api.bscscan.com/api?module=account&action=tokentx&address={WALLET_ADDR}&page=1&offset=0&startblock=0&endblock=999999999&sort=asc&apikey={API_KEY}"
+
+print('Retrieving Transactions from API ...')
+response = requests.get(TRANSACTION_URL)
+allTransactions = response.json()['result']
+
+print('Sorting transactions per contact ...')
+for transaction in allTransactions:
+    for contract in allContracts:
+        #check if the transaction.contractAddress matches contract.address, add to contract.transaction[]
+        if transaction['contractAddress'] == contract['address']:
+            contract['transactions'].append(transaction)
+            break
+
+print('Setting the QTY value ...')
+for contract in allContracts:
+    QTY_URL = f"https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress={contract['address']}&address={WALLET_ADDR}&tag=latest&apikey={API_KEY}"
+
+    response = requests.get(QTY_URL)
+    data = response.json()
+    api_qty = data['result']
+    if api_qty == "0":
+        qty = 0
+    else:
+        qty = data['result'].strip("0")
+        qty = int(qty)
+    contract['qty'] = qty
+
+allContracts = json.dumps(allContracts)
+
+print('Updating contracts.json file ...')
+
+# write new contracts with updated value to contracts.json file
+with open('contracts.json', 'w') as file:
+    file.write(allContracts)
+
+print('contracts.json file updated')
+
+print('Pushing the contract.json file to github ...')
+# push contracts.json to git
+# os.system("git add contracts.json")
+# os.system("git commit -m 'updated contracts.json'")
+# os.system("git push")
+print ('Done')
