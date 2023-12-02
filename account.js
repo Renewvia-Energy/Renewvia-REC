8213ef9025afa9e39a7ae56944f932a5aeb7dedf
Nicholas Selby <rupumped@gmail.com>
2023-12-02 15:04:28 +0300

    testing ignore and notes in contracts.json

--- account.js
+++ account.js
@@ -2,9 +2,13 @@ const WALLET = window.location.href.split("?")[1];
 const CONTRACTS_URL = "https://renewvia-energy.github.io/Renewvia-REC/contracts.json";
 const COMPANIES_URL = "https://renewvia-energy.github.io/Renewvia-REC/companies.json";
 
+function strcmpi(str1, str2) {
+	return str1.localeCompare(str2, undefined, {sensitivity: 'base'}) === 0
+}
+
 const app = Vue.createApp({
-	data() {
-		account = {
+	data() {	
+		return {
 			name: '',
 			address: WALLET,
 			smallAddress: WALLET.substring(0,4)+'...'+WALLET.substring(WALLET.length-4),
@@ -12,44 +16,90 @@ const app = Vue.createApp({
 			join_date: '',
 			assets: [],
 			activity: [],
-			carbonAssetsData: [],
-			companyData: [],
-			doughnutData: {},
-			chart: null,
-			totalRenewableEnergy: 0,
-			totalCarbonOffsets: 0,
+			chart: null
 		}
-
-		fetch(COMPANIES_URL)
-			.then((companiesResp) => companiesResp.json())
-			.then((companies) => {
-				for (company of companies) {
-					if (company.address.toLowerCase() === WALLET) {
-						account.name = company['name']
-						account.logo = company['logo']
-						account.join_date = company['join_date']
-						break
-					}
-				}
-				fetch(contracts_url)
-					.then((allContractsResp) => allContractsResp.json())
-					.then((allContractsData) => {
-						for (let contract of allContractsData) {
-							for (let trans of contract['transactions']) {
-								if (trans['to'] === WALLET || trans['from'] === WALLET) {
-									
-								}
-							}
-						}
-
-						return account
-					})
-			})
-		
 	},
 
 	mounted() {
-		this.statesData();
+		fetch(COMPANIES_URL)
+		.then((companiesResp) => companiesResp.json())
+		.then((companies) => {
+			// Find my company in the list of companies
+			for (company of companies) {
+				if (company.address.toLowerCase() === WALLET.toLowerCase()) {
+					this.name = company['name']
+					this.logo = company['logo']
+					this.join_date = company['join_date']
+					break
+				}
+			}
+			// If not registered, load default values
+			if (!this.name) {
+				this.name = 'R-REC User'
+				this.logo = 'https://static.wixstatic.com/media/f3e4c8_605e4a0db8f94c88ba747298bcdf3648~mv2.png/v1/crop/x_105,y_113,w_814,h_812/fill/w_812,h_812,al_c,q_90,enc_auto/Renewvia%20logo_50.png'
+				this.join_date = 'Yet to be Verified'
+			}
+			// Scrape the contracts for transactions involving this wallet
+			fetch(CONTRACTS_URL)
+				.then((allContractsResp) => allContractsResp.json())
+				.then((allContractsData) => {
+					// For each contract
+					for (let contract of allContractsData) {
+						// For each transaction of that contract
+						for (let trans of contract['transactions']) {
+							if (trans.ignore) {
+								continue
+							}
+							// If this wallet is involved in the transaction
+							if (strcmpi(trans['to'], WALLET) || strcmpi(trans['from'], WALLET)) {
+								// Make the amount negative if this wallet is the sender
+								trans.signedAmount = strcmpi(trans['from'], WALLET) ? trans.amount*-1 : trans.amount
+
+								// Add the transaction to the activity table
+								switch (trans.action) {
+									case 'transfer':
+										trans.action = strcmpi(trans['from'], WALLET) ? 'Sale' : 'Purchase'
+										break
+									case 'mint':
+										trans.action = 'Generation'
+										break
+									default:
+										trans.action = trans.action.charAt(0).toUpperCase() + trans.action.slice(1)
+								}
+								trans.name = contract.name
+								let date = new Date(trans.timeStamp*1000)
+								trans.date = `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
+								this.activity.push(trans)
+
+								// Increment asset count
+								let ind = -1
+								for (let a=0; a<this.assets.length; a++) {
+									if (this.assets[a].address == contract.address) {
+										ind = a
+										break
+									}
+								}
+								if (ind == -1) {
+									this.assets.push({
+										name: contract.name,
+										abbreviation: contract.abbreviation,
+										superclass: contract.superclass,
+										address: contract.address,
+										signedAmount: trans.signedAmount,
+										amount: trans.amount,
+										img: contract.img,
+										description: contract.description
+									})
+								} else {
+									this.assets[ind].amount+= trans.signedAmount
+								}
+							}
+						}
+					}
+					this.assets = this.assets.filter((asset) => asset.amount>0)
+					this.activity.sort((action1, action2) => action1.timeStamp-action2.timeStamp)
+				})
+		})
 	},
 
 	updated() {
@@ -59,61 +109,31 @@ const app = Vue.createApp({
 		}
 	},
 
+	computed: {
+		totalRenewableEnergy() {
+			return this.assets.reduce((sum, asset) => asset.superclass === 'REC' ? sum+asset.amount : sum, 0)
+		},
+
+		totalCarbonOffsets() {
+			return this.assets.reduce((sum, asset) => asset.superclass === 'CC' ? sum+asset.amount : sum, 0)
+		}
+	},
+
 	methods: {
-		statesData() {
-			const walletAddress = window.location.href.split("?")[1];
-			const contracts_url = "https://renewvia-energy.github.io/Renewvia-REC/contracts.json";
-			const companies_url = "https://renewvia-energy.github.io/Renewvia-REC/companies.json";
+		calculatePercentage(quantity) {
+			// Calculate the percentage here based on the total quantity
+			const totalQuantity = this.assets.reduce((total, asset) => total + asset.amount, 0)
+			return Math.round(100 * quantity / totalQuantity)
+		},
 
-			// Get all the contracts from json in github
-			fetch(contracts_url)
-				.then((allContractsResp) => allContractsResp.json())
-				.then((allContractsData) => {
-					// loop through each contract and add key for qyt and transactions
-					for (let i = 0; i < allContractsData.length; i++) {
-						// set the key values for qty to 0 and transaction to empty array
-						allContractsData[i]["chartData"] = {};
-					}
-					
-					// filter the contracts if company address is passed in the url
-					if (walletAddress) {
-
-
-						this.carbonAssetsData = allContractsData.filter(
-							(asset) => asset.company_address === walletAddress
-						);
-							// getting company data
-							fetch(companies_url)
-							.then((allCompaniesResp) => allCompaniesResp.json())
-							.then((allCompanyData) => {
-								// putting ellipsis in between company address
-								for (let i = 0; i < allCompanyData.length; i++) {
-									if (allCompanyData[i].address === walletAddress) {
-										let smallAdress = allCompanyData[i].address;
-										let startSubStr = smallAdress.substr(0, 4)
-										let endSubStr = smallAdress.substr(-4)
-										smallAdress = startSubStr+'...'+endSubStr
-										allCompanyData[1]['smallAddress'] = smallAdress
-										this.companyData.push(allCompanyData[i]);
-									}
-								}
-							});
-						}
-						// if no company address is passed on the url return all the contracts
-						if (!walletAddress) {
-							this.carbonAssetsData = allContractsData;
-					}
-
-					// call function(method) to calculate renewable energy Total
-					this.calculateRenewableEnergy();
-
-					//passing the data to the chart
-					for (let contract of this.carbonAssetsData) {
-						const labels = this.carbonAssetsData.map((asset) => asset.name);
-						const quantities = this.carbonAssetsData.map((asset) => asset.qty);
-
-						this.doughnutData = {
-							labels: labels,
+		renderChart() {
+			if (this.$refs.myChart && this.assets && this.assets.length > 0) {
+				const ctx = this.$refs.myChart.getContext("2d");
+				if (ctx) {
+					this.chart = new Chart(ctx, {
+						type: "doughnut",
+						data: {
+							labels: this.assets.map((asset) => asset.name),
 							datasets: [
 								{
 									backgroundColor: [
@@ -122,47 +142,10 @@ const app = Vue.createApp({
 										"#ff6d05",
 										"rgb(218, 148, 68)",
 									], // Add colors as needed
-									data: quantities,
-								},
-							],
-						};
-						contract.chartData = this.doughnutData;
-					}
-				});
-		},
-		calculatePercentage(quantity) {
-			// Calculate the percentage here based on the total quantity
-			if (quantity === 0) {
-				return 0;
-			}
-			const totalQuantity = this.carbonAssetsData.reduce(
-				(total, asset) => total + asset.qty,
-				0
-			);
-			const answer = Math.round((quantity / totalQuantity) * 100);
-			return answer;
-		},
-
-		calculateRenewableEnergy() {
-			let total = 0;
-			for (let asset of this.carbonAssetsData) {
-				// calculating renewable energy total using hardcoded value
-				total = total + asset.qty;
-			}
-			this.totalRenewableEnergy = total;
-		},
-		renderChart() {
-			if (
-				this.$refs.myChart &&
-				this.carbonAssetsData &&
-				this.carbonAssetsData.length > 0
-			) {
-				const ctx = this.$refs.myChart.getContext("2d");
-				if (ctx) {
-					this.chart = new Chart(ctx, {
-						type: "doughnut",
-						data: this.carbonAssetsData[this.carbonAssetsData.length - 1]
-							.chartData,
+									data: this.assets.map((asset) => asset.amount),
+								}
+							]
+						},
 						options: {
 							responsive: true,
 							maintainAspectRatio: false,
@@ -170,10 +153,8 @@ const app = Vue.createApp({
 					});
 				}
 			}
-		},
-		toggleMenu() {
-			this.isHidden = !this.isHidden;
-		},
+		}
 	},
 });
+
 app.mount("#app");
