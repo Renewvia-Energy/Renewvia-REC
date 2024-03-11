e6e6fc41f2f5b3c69f2fc2848f45190e33acc4ca
Nicholas Selby <rupumped@gmail.com>
2024-03-11 13:03:59 +0300

    tabs

--- deployed contracts/RenewviaREC-NC.sol
+++ deployed contracts/RenewviaREC-NC.sol
@@ -7,27 +7,27 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact technical-africa@renewvia.com
 contract RenewviaREC is ERC20, Pausable, Ownable {
-    constructor(uint256 premint) ERC20("RenewviaREC-NorthCarolina", "RREC-NC") {
-        _mint(msg.sender, premint * 10 ** decimals());
-    }
+	constructor(uint256 premint) ERC20("RenewviaREC-NorthCarolina", "RREC-NC") {
+		_mint(msg.sender, premint * 10 ** decimals());
+	}
 
-    function pause() public onlyOwner {
-        _pause();
-    }
+	function pause() public onlyOwner {
+		_pause();
+	}
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
+	function unpause() public onlyOwner {
+		_unpause();
+	}
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
-    }
+	function mint(address to, uint256 amount) public onlyOwner {
+		_mint(to, amount * 10 ** decimals());
+	}
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override
-    {
-        super._beforeTokenTransfer(from, to, amount);
-    }
+	function _beforeTokenTransfer(address from, address to, uint256 amount)
+		internal
+		whenNotPaused
+		override
+	{
+		super._beforeTokenTransfer(from, to, amount);
+	}
 }
