20157c785f86d68c7310baa33fe1ee63a678e5c6
Nicholas Selby <rupumped@gmail.com>
2025-01-16 13:30:27 +0300

    solarly

--- deployed_contracts/RenewviaREC-ACM.sol
+++ deployed_contracts/RenewviaREC-ACM.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
 contract RenewviaREC is ERC20, ERC20Pausable, Ownable {
     event MintWithInfo(address indexed to, uint256 amount, string additionalInfo);
     constructor(address initialOwner)
-        ERC20("RenewviaREC", "RREC")
+        ERC20("RenewviaREC-ACM", "RREC-ACM")
         Ownable(initialOwner)
     { }
 
@@ -26,7 +26,6 @@ contract RenewviaREC is ERC20, ERC20Pausable, Ownable {
     }
 
     // The following functions are overrides required by Solidity.
-
     function _update(address from, address to, uint256 value)
         internal
         override(ERC20, ERC20Pausable)
