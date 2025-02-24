7d0353f45487294a4d9500fc6e638b71e338b099
rupumped <rupumped@gmail.com>
2025-02-24 17:56:43 +0300

    ready for blacklist

--- contracts/src/templates/RenewviaREC.sol
+++ contracts/src/templates/RenewviaREC.sol
@@ -1,16 +1,31 @@
 // SPDX-License-Identifier: GPLv3
-pragma solidity ^0.8.20;
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
 
-import "./openzeppelin/ERC20.sol";
-import "./openzeppelin/ERC20Pausable.sol";
-import "./openzeppelin/Ownable.sol";
+import {ERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import {ERC20PausableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
+import {ERC20PermitUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract RenewviaREC is ERC20, ERC20Pausable, Ownable {
+/// @custom:security-contact recs@renewvia.com
+contract RenewviaREC is Initializable, ERC20Upgradeable, ERC20PausableUpgradeable, OwnableUpgradeable, ERC20PermitUpgradeable, UUPSUpgradeable {
 	event MintWithInfo(address indexed to, uint256 amount, string additionalInfo);
-	constructor(address initialOwner)
-		ERC20("RenewviaREC", "RREC")
-		Ownable(initialOwner)
-	{ }
+
+	/// @custom:oz-upgrades-unsafe-allow constructor
+	constructor() {
+		_disableInitializers();
+	}
+
+	function initialize(address initialOwner) public initializer {
+		require(initialOwner != address(0), "Invalid owner address");
+		__ERC20_init("RenewviaREC", "RREC");
+		__ERC20Pausable_init();
+		__Ownable_init(initialOwner);
+		__ERC20Permit_init("RenewviaREC");
+		__UUPSUpgradeable_init();
+	}
 
 	function pause() public onlyOwner {
 		_pause();
@@ -21,13 +36,27 @@ contract RenewviaREC is ERC20, ERC20Pausable, Ownable {
 	}
 
 	function mint(address to, uint256 amount, string calldata additionalInfo) public onlyOwner {
-		_mint(to, amount * 10 ** decimals());
-		emit MintWithInfo(to, amount, additionalInfo);
-	}
+        require(to != address(0), "Cannot mint to zero address");
+        require(amount > 0, "Amount must be positive");
+        
+        uint256 amountWithDecimals = amount * (10 ** decimals());
+        require(amountWithDecimals / (10 ** decimals()) == amount, "Amount overflow");
+        
+        _mint(to, amountWithDecimals);
+        emit MintWithInfo(to, amount, additionalInfo);
+    }
+
+	function _authorizeUpgrade(address newImplementation)
+		internal
+		override
+		onlyOwner
+	{}
+
+	// The following functions are overrides required by Solidity.
 
 	function _update(address from, address to, uint256 value)
 		internal
-		override(ERC20, ERC20Pausable)
+		override(ERC20Upgradeable, ERC20PausableUpgradeable)
 	{
 		super._update(from, to, value);
 	}
