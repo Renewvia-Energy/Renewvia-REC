b7179569cddbdccdbaec24cd2436ca30d052cb99
rupumped <rupumped@gmail.com>
2025-03-14 13:15:42 +0300

    fix forge install script

--- contracts/install.sh
+++ contracts/install.sh
@@ -1,6 +1,3 @@
-# forge install foundry-rs/forge-std
-# forge install OpenZeppelin/openzeppelin-foundry-upgrades@v0.4.0
-# forge install OpenZeppelin/openzeppelin-contracts-upgradeable@v5.2.0
-# forge install OpenZeppelin/openzeppelin-contractsv5.2.0
-git submodule init
-git submodule update
+forge install foundry-rs/forge-std
+forge install OpenZeppelin/openzeppelin-foundry-upgrades@v0.4.0
+forge install OpenZeppelin/openzeppelin-contracts-upgradeable@v5.2.0
